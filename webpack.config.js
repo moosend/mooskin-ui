be90d52d9d525dd1f3022aa708be516471ddfd7e
Gent Gashi <gentgaashi@gmail.com>
2017-05-19 16:05:17 +0200

    Removed Source maps

--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,6 @@ module.exports = {
  module: {
    rules: [
      {
-       enforce: 'pre',
-       test: /\.js$/,
-       loader: "source-map-loader"
-     },
-     {
-       enforce: 'pre',
-       test: /\.tsx?$/,
-       use: "source-map-loader"
-     },
-     {
        test: /\.tsx?$/,
        exclude: /node_modules/,
        use: [ 'babel-loader', 'ts-loader' ]
