ec73a736cdb0ca6f4c397a4c4b6d0ac38191f0b6
donibehrami <donibehrami@gmail.com>
2017-07-21 10:52:19 +0200

    added to index.ts

--- components/index/index.ts
+++ components/index/index.ts
@@ -6,6 +6,7 @@ export {default as TextArea} from '../TextArea';
 export {default as Select, Option} from '../Select';
 export {default as Switch} from '../Switch';
 export {H1, H2, H3, H4, H5, H6} from '../Headings';
+export {default as Fieldset} from '../Fieldset';
 export {default as HorizontalRangeBar} from '../HorizontalRangeBar';
 export {default as TopNotification} from '../TopNotification';
 export {default as SmallIconButton} from '../SmallIconButton';
