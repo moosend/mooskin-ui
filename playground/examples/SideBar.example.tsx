531373da644418f9097d88124dcff7cc362d5e95
donibehrami <donibehrami@gmail.com>
2017-08-04 12:32:01 +0200

    SideBar live edit.

--- playground/examples/SideBar.example.tsx
+++ playground/examples/SideBar.example.tsx
@@ -1,99 +1,22 @@
 import * as React from 'react';
 
-import SideBar, {Item} from '../../components/SideBar';
+import Readme from '../../components/SideBar/README.md';
 
-export interface ISideBarState{
-    sidebar1: boolean;
-    sidebar2: boolean;
-    sidebar3: boolean;
-}
+import {Item, SideBar} from '../../components/index/';
 
-export default class SideBarExample extends React.Component<{}, ISideBarState>{
+import SideBarExampleCode from './component-strings/SideBar.example.txt';
 
-    constructor(props: any){
-        super(props);
+import ReactLiveEditor from '../tools/ReactLiveEditor/ReactLiveEditor';
 
-        this.state = {
-            sidebar1: false,
-            sidebar2: false,
-            sidebar3: false
-        };
-    }
-
+export default class SideBarExample extends React.Component<any, any> {
     public render(){
         return(
-            <SideBar display={this.state.sidebar1} onClick={this.onButtonClick} button >
-                <Item
-                    image="http://daynin.github.io/clojurescript-presentation/img/react-logo.png"
-                    label="Automations"
-                    href="automations"
-                />
-                <Item
-                    image="http://hguochen.com/static/img/tech/css3.png"
-                    label="Mailing List"
-                    href="mailing/list"
-                />
-                <Item
-                    image="https://www.easyprogramming.net/logo/js.png"
-                    label="SideBar 2"
-                    onMouseEnter={this.sidebarTwo}
-                    onMouseLeave={this.sidebarTwo}
-                >
-                    <SideBar
-                        display={this.state.sidebar2}
-                        style={{marginLeft: 100, background: '#efefef', color: '#000'}}
-                    >
-                        <Item
-                            label="SideBar 3"
-                            onMouseEnter={this.sidebarThree}
-                            onMouseLeave={this.sidebarThree}
-                        >
-                            <SideBar
-                                display={this.state.sidebar3}
-                                style={{marginLeft: 200}}
-                            >
-                                <Item
-                                    image="https://www.easyprogramming.net/logo/js.png"
-                                    href="https://www.google.com"
-                                    label="Automations"
-                                />
-                                <Item
-                                    image="http://hguochen.com/static/img/tech/css3.png"
-                                    label="Mailing List"
-                                    href="mailing/list"
-                                />
-                                <Item
-                                    image="http://daynin.github.io/clojurescript-presentation/img/react-logo.png"
-                                    label="Campaigns"
-                                />
-                            </SideBar>
-                        </Item>
-                        <Item
-                            label="Mailing List"
-                        />
-                        <Item
-                            label="Campaigns"
-                        />
-                    </SideBar>
-                </Item>
-            </SideBar>
+            <ReactLiveEditor
+                scope={{React, SideBar, Item}}
+                code={SideBarExampleCode}
+                title="SideBar Example"
+                doc={Readme}
+            />
         );
     }
-
-    private onButtonClick = () => {
-        this.setState({sidebar1: true});
-    }
-
-    private sidebarTwo = () => {
-        // console.log(name);
-        this.setState({sidebar2: !this.state.sidebar2});
-    }
-
-    private sidebarThree = () => {
-        // console.log(name);
-        this.setState({sidebar3: !this.state.sidebar3});
-    }
-
 }
-
-
