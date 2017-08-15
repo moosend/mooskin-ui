3f081e08615c1257c3eed718a8fa09a46d075cd5
donibehrami <donibehrami@gmail.com>
2017-08-15 12:31:49 +0200

    test for onClick callback

--- components/Table/Table.spec.tsx
+++ components/Table/Table.spec.tsx
@@ -6,7 +6,6 @@ import { shallow } from 'enzyme';
 describe('Table', () => {
 
     test('renders correctly', () => {
-        const func = jest.fn();
 
         const tree = shallow(
             <Table>
@@ -32,6 +31,39 @@ describe('Table', () => {
             </Table>
         );
         expect(tree).toMatchSnapshot();
+    });
+
+    test('callback when header is clicked', () => {
+
+        const func = jest.fn();
+
+        const tree = shallow(
+            <Table>
+                <THead>
+                    <TR>
+                        <TH onClick = {func}>th 1</TH>
+                        <TH>th 2</TH>
+                        <TH>th 3</TH>
+                    </TR>
+                </THead>
+                <TBody>
+                    <TR>
+                        <TD>td 1</TD>
+                        <TD>td 2</TD>
+                        <TD>td 3</TD>
+                    </TR>
+                    <TR>
+                        <TD>td 1</TD>
+                        <TD>td 2</TD>
+                        <TD>td 3</TD>
+                    </TR>
+                </TBody>
+            </Table>
+        );
+
+        tree.find(TH).first().simulate('click');
+        expect(func).toHaveBeenCalled();
+
     });
 
 });
