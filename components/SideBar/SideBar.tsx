56744245c89fc0af0fa6d47b2f3bb2138434f3ff
donibehrami <donibehrami@gmail.com>
2017-09-20 10:35:53 +0200

    Fix for Sidebar active arrow not appearing on Items

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -45,6 +45,9 @@ export interface ISideBarItemProps{
     /** if active */
     active?: boolean;
 
+    /** which subMenu is active to apply classes to the Item */
+    activeSubMenu?: boolean;
+
     /** sidebar class */
     className?: string;
 
@@ -193,6 +196,7 @@ export default class SideBar extends React.Component<ISideBarProps, ISideBarStat
                             onMouseLeave={this.subMenuOff}
                             style={child.props.style}
                             className={child.props.className}
+                            activeSubMenu={this.state.activeSubMenu === index}
                         />
                     );
                 } else {
@@ -398,7 +402,7 @@ export const Item: React.StatelessComponent<ISideBarItemProps> = (props) => {
     };
 
     const activeItem = props.active ? styles.activeItem : '';
-    const arrow = props.children ? styles.arrow : '';
+    const arrow = props.activeSubMenu ? styles.arrow : '';
     const arrowStyle = props.image ? styles.arrowImg : styles.arrowNoImg;
 
     return(
@@ -417,9 +421,6 @@ export const Item: React.StatelessComponent<ISideBarItemProps> = (props) => {
                 <img src={getImage()} className={styles.image} />
                 <span>{props.label}</span>
             </a>
-            <div>
-                {props.children}
-            </div>
         </div>
     );
 };
