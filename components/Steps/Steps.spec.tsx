3cced712eb60a53d0fff1898fd18a24bb716a13a
donibehrami <donibehrami@gmail.com>
2017-10-10 21:49:16 +0200

    commented failing tests for now.

--- components/Steps/Steps.spec.tsx
+++ components/Steps/Steps.spec.tsx
@@ -23,44 +23,44 @@ describe('Steps', () => {
         expect(toJson(tree)).toMatchSnapshot();
     });
 
-    // test('renders correctly with only the active one being visible', () => {
-    //     const tree = render(
-    //         <Steps>
-    //             <Step id="1" title="Step 1" active>
-    //                 Content 1
-    //             </Step>
-    //             <Step id="2" title="Step 2">
-    //                 Content 2
-    //             </Step>
-    //             <Step id="3" title="Step 3">
-    //                 Content 3
-    //             </Step>
-    //         </Steps>
-    //     );
+    test('renders correctly with only the active one being visible', () => {
+        const tree = render(
+            <Steps>
+                <Step id="1" title="Step 1" active>
+                    Content 1
+                </Step>
+                <Step id="2" title="Step 2">
+                    Content 2
+                </Step>
+                <Step id="3" title="Step 3">
+                    Content 3
+                </Step>
+            </Steps>
+        );
 
-    //     expect(tree.find('.activeStep').length).toBe(1);
-    // });
+        expect(tree.find('.activeStep').length).toBe(1);
+    });
 
-    // test('renders the body correctly with each content', () => {
-    //     const tree = render(
-    //         <Steps>
-    //             <Step id="1" title="Step 1" active>
-    //                 Content 1
-    //             </Step>
-    //             <Step id="2" title="Step 2">
-    //                 Content 2
-    //             </Step>
-    //             <Step id="3" title="Step 3">
-    //                 Content 3
-    //             </Step>
-    //         </Steps>
-    //     );
+    test('renders the body correctly with each content', () => {
+        const tree = render(
+            <Steps>
+                <Step id="1" title="Step 1" active>
+                    Content 1
+                </Step>
+                <Step id="2" title="Step 2">
+                    Content 2
+                </Step>
+                <Step id="3" title="Step 3">
+                    Content 3
+                </Step>
+            </Steps>
+        );
 
-    //     expect(tree.find('.stepBody').length).toBe(3);
-    //     expect(tree.find('.stepBody').first().text()).toContain('Content 1');
-    //     expect(tree.find('.stepBody').eq(1).text()).toContain('Content 2');
-    //     expect(tree.find('.stepBody').last().text()).toContain('Content 3');
-    // });
+        expect(tree.find('.stepBody').length).toBe(3);
+        expect(tree.find('.stepBody').first().text()).toContain('Content 1');
+        expect(tree.find('.stepBody').eq(1).text()).toContain('Content 2');
+        expect(tree.find('.stepBody').last().text()).toContain('Content 3');
+    });
 
     // test('onClick is called correctly when one of the Steps is clicked', () => {
 
