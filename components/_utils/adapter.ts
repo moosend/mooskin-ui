56b0116299915c8c8742f751fd292bf4bfde6a9b
Doni Behrami <donibehrami@gmail.com>
2019-03-20 17:25:55 +0100

    Upgrade TS Jest

--- components/_utils/adapter.ts
+++ components/_utils/adapter.ts
@@ -1,5 +1,5 @@
-import { configure } from 'enzyme';
+import * as Enzyme from 'enzyme';
 
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
