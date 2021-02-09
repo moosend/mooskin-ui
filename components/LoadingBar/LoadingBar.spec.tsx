f95337ebd69bf4330061849ce51bac923bd6d57a
Doni Behrami <doni@moosend.com>
2021-02-09 11:22:44 +0100

    add loading bar component

---
+++ components/LoadingBar/LoadingBar.spec.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+
+import Button from '../Button/Button';
+import LoadingBar from './LoadingBar';
+
+import {shallow} from 'enzyme';
+
+describe('LoadingBar', () => {
+
+    test(`LoadingBar renders correctly`, () => {
+        const func1 = jest.fn();
+        const func2 = jest.fn();
+
+        const tree = shallow(
+            <LoadingBar
+                progress={10}
+                error={false}
+                onLoaderError={func1}
+                onLoaderDone={func2}
+            />
+        );
+
+        expect(tree).toMatchSnapshot();
+
+    });
+
+    test(`calls done callback function on complete`, () => {
+        const func = jest.fn();
+
+        let progress = 80;
+
+        const component = shallow(
+            <LoadingBar
+                progress={progress}
+                error={false}
+                onLoaderDone={func}
+            />
+        );
+
+        component.setProps({progress: 100});
+
+        setTimeout(() => {
+            expect(func).toHaveBeenCalled();
+        }, 10);
+    });
+
+    test(`onLoaderError callback is called when an error prop of true is passed`, () => {
+        const func = jest.fn();
+
+        const component = shallow(
+            <LoadingBar
+                progress={10}
+                onLoaderError={func}
+            />
+        );
+
+        component.setProps({error: true});
+
+        setTimeout(() => {
+            expect(func).toHaveBeenCalled();
+        }, 10);
+
+    });
+
+});
---
+++ components/LoadingBar/LoadingBar.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+// Models
+import { ILoadingBarComponentProps } from './model';
+
+// Styled Components
+import { StyledLoadingBar } from './styles';
+
+export const LoadingBar: React.FC<ILoadingBarComponentProps> = (props) => {
+
+    const [progress, setProgress] = React.useState(props.progress);
+    const [opacity, setOpacity] = React.useState(1);
+
+    React.useEffect(() => {
+        setProgress(props.progress);
+    }, [props.progress]);
+
+    React.useEffect(() => {
+        if (props.error){
+            props.onLoaderError && props.onLoaderError();
+            setProgress(100);
+        }
+    }, [props.error]);
+
+    React.useEffect(() => {
+        if (progress >= 100){
+            !props.error && props.onLoaderDone && props.onLoaderDone();
+            setTimeout(() => {
+                setOpacity(0);
+            }, 500);
+            setTimeout(() => {
+                setProgress(0);
+            }, 1000);
+            setTimeout(() => {
+                setOpacity(1);
+            }, 1500);
+        }
+    }, [progress]);
+
+    return <StyledLoadingBar {...props} progress={progress} opacity={opacity} />;
+
+};
+
+LoadingBar.defaultProps = {
+    className: '',
+    progress: 0,
+    style: {}
+};
+
+LoadingBar.displayName = 'LoadingBar';
+
+export default LoadingBar;
