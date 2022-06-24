80e570982fd7713710c8dd23e7b3aa8c9910f447
Gent Spahiu <ges@sitecore.net>
2022-06-24 13:22:33 +0200

    Adjust Datepicker colors for Moosend theme. Share same config for D and DT pickers

--- components/_utils/helper.ts
+++ components/_utils/helper.ts
@@ -1,4 +1,5 @@
 import { boxComponentProps } from '../Box/model';
+import { IStyledTheme } from '../Styled/model';
 
 export const arrayHasDupes = (array: any[]): boolean => {
 	const n = array.length;
@@ -47,3 +48,73 @@ export const getBoxProps = (componentProps: any) => {
 	});
 	return boxProps;
 };
+
+export const getOverridesForPicker = (theme: IStyledTheme, defaults: IStyledTheme) => ({
+	overrides: {
+		MuiPickersToolbar: {
+			toolbar: {
+				backgroundColor: theme.backgroundColors.primary1 || defaults.backgroundColors.primary1
+			}
+		},
+		MuiPickersCalendarHeader: {
+			iconButton: {
+				fill: theme.fontColors.text || defaults.fontColors.text
+			}
+		},
+		MuiPickersDay: {
+			current: {
+				color: theme.fontColors.white || defaults.fontColors.white
+			},
+			day: {
+				backgroundColor: theme.backgroundColors.white || defaults.backgroundColors.white,
+				color: theme.fontColors.text || defaults.fontColors.text
+			},
+			dayDisabled: {
+				color: theme.backgroundColors.medgray1 || defaults.backgroundColors.medgray1
+			},
+			daySelected: {
+				backgroundColor: theme.backgroundColors.primary1 || defaults.backgroundColors.primary1,
+				color: theme.fontColors.white || defaults.fontColors.white
+			}
+		},
+		MuiPickersModal: {
+			dialogAction: {
+				color: theme.fontColors.text || defaults.fontColors.text
+			}
+		},
+		MuiPickerDTTabs: {
+			tabs: {
+				backgroundColor: theme.backgroundColors.primary1 || defaults.backgroundColors.primary1,
+				fill: theme.fontColors.white || defaults.fontColors.white
+			}
+		},
+		MuiSvgIcon: {
+			root: {
+				fill: 'inherit'
+			}
+		},
+		MuiPickersClock: {
+			pin: {
+				backgroundColor: theme.backgroundColors.primary1 || defaults.backgroundColors.primary1
+			}
+		},
+		MuiPickersClockPointer: {
+			pointer: {
+				backgroundColor: theme.backgroundColors.primary1 || defaults.backgroundColors.primary1
+			},
+			thumb: {
+				borderColor: theme.backgroundColors.primary1 || defaults.backgroundColors.primary1
+			}
+		},
+		MuiTabs: {
+			indicator: {
+				backgroundColor: theme.backgroundColors.primary3 || defaults.backgroundColors.primary3
+			}
+		},
+		MuiIconButton: {
+			label: {
+				color: 'inherit'
+			}
+		}
+	}
+});
