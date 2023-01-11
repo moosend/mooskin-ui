ab3e86d537bbd9086aba022f5ac2520fbd8cbb36
Doni Behrami <donb@sitecore.net>
2023-01-11 19:21:38 +0100

    text editor initialization fix

--- components/TextEditor/TextEditor.tsx
+++ components/TextEditor/TextEditor.tsx
@@ -42,7 +42,7 @@ export const TextEditor: React.FC<ITextEditorComponentProps> = (props) => {
 		<Editor
 			{...props}
 			toolbar={getToolbar()}
-			onInit={(evt, editor) => props.onInit(editor)}
+			onInit={(evt, editor) => props.onInit && props.onInit(editor)}
 			init={{
 				setup: props.personalizationTags ? getSetup : undefined,
 				...props.init,
